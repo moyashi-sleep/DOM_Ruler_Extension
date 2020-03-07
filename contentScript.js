1c9bbd4566fda43de912995bf0c748cca6b74449
moyashi-sleep <53850092+moyashi-sleep@users.noreply.github.com>
2020-03-07 18:46:29 +0900

    [Add]Canvas表示処理を追加
    ・Canvasを表示する処理を追加しました。
    ・一部描画処理を仮実装しました。

--- contentScript.js
+++ contentScript.js
@@ -1,16 +1,21 @@
 
+const canvas = $("<canvas>").attr("id", "DOM-Measure-Extension-Canvas");
+const canvasWrapper = $("<div>").attr("id", "DOM-Measure-Extension-Canvas-Wrapper");
+const context = canvas.get(0).getContext("2d");
+
 // popupからのメッセージを受信したとき
 chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
   console.log(message.isEnabled);
   if (message.isEnabled) {
     // canvas要素を追加する
+    $("body").prepend(canvasWrapper.append(canvas));
+    canvas.attr({"width": canvasWrapper.width(), "height": canvasWrapper.height()});
     // クリック時のイベントハンドラを登録する
     activateEventListener();
   } else {
     // canvas要素を削除する
     // イベントハンドラを削除する
     $(window).off("click mouseenter");
-
   }
 });
 
@@ -41,34 +46,52 @@ function activateEventListener() {
 
         console.log("target", targetPoint);
 
+        const distance = {};
         // Bの右辺位置がAの左辺位置より小さかったら左確定、逆および上下も同様
         // Bの右辺位置がAの左辺位置より大きかったら左辺との差で確定、逆も同様
         // ターゲットが上に位置する場合
         if (basePoint.top > targetPoint.bottom) {
-          console.log(Math.abs(basePoint.top - targetPoint.bottom));
+          distance.top = Math.abs(basePoint.top - targetPoint.bottom);
           // BaseとTargetは接触しないので、上への線のみ
+          context.clearRect(0, 0, canvasWrapper.width(), canvasWrapper.height());
+          const x = (basePoint.left + basePoint.right) / 2 - window.scrollX;
+          const startY = basePoint.top - window.scrollY;
+          const endY = targetPoint.bottom - window.scrollY;
+          context.beginPath();
+          context.moveTo(x, startY);
+          context.lineTo(x, endY);
+          context.closePath();
+          context.stroke();
         } else {
-          console.log(basePoint.top - targetPoint.top > 0 ? basePoint.top - targetPoint.top : 0);
+          distance.top = basePoint.top - targetPoint.top > 0 ? basePoint.top - targetPoint.top : 0;
         }
         // ターゲットが下に位置する場合
         if (basePoint.bottom < targetPoint.top) {
-          console.log(Math.abs(basePoint.bottom - targetPoint.top));
+          distance.bottom = Math.abs(basePoint.bottom - targetPoint.top);
         } else {
-          console.log(targetPoint.bottom - basePoint.bottom > 0 ? targetPoint.bottom - basePoint.bottom : 0);
+          distance.bottom = targetPoint.bottom - basePoint.bottom > 0 ? targetPoint.bottom - basePoint.bottom : 0;
         }
         // ターゲットが左に位置する場合
         if (basePoint.left > targetPoint.right) {
-          console.log(Math.abs(basePoint.left - targetPoint.right));
+          distance.left = Math.abs(basePoint.left - targetPoint.right);
         } else {
-          console.log(basePoint.left - targetPoint.left > 0 ? basePoint.left - targetPoint.left : 0);
+          distance.left = basePoint.left - targetPoint.left > 0 ? basePoint.left - targetPoint.left : 0;
         }
         // ターゲットが右に位置する場合
         if (basePoint.right < targetPoint.left) {
-          console.log(Math.abs(basePoint.right - targetPoint.left));
+          distance.right = Math.abs(basePoint.right - targetPoint.left);
         } else {
-          console.log(targetPoint.right - basePoint.right > 0 ? targetPoint.right - basePoint.right : 0);
+          distance.right = targetPoint.right - basePoint.right > 0 ? targetPoint.right - basePoint.right : 0;
         }
+        console.log($target.get(0).tagName, distance);
+      },
+      mouseleave: function(hoverEvent) {
+        context.clearRect(0, 0, canvasWrapper.width(), canvasWrapper.height());
       }
     });
+  });
+
+  $(window).on("scroll", function() {
+    // 再描画する
   });
 }
