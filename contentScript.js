9940f61b17325a3ebd0e45c225783a6f6802b8ca
moyashi-sleep <53850092+moyashi-sleep@users.noreply.github.com>
2020-03-15 16:26:09 +0900

    [Add]接触しないオブジェクト間のルーラー描画処理を実装
    ・接触しないオブジェクト間にルーラーを描画する処理を実装しました。
    　「接触しているが内包されない」パターンは未実装です。

--- contentScript.js
+++ contentScript.js
@@ -16,6 +16,10 @@ function Rect(element) {
     bottom: rectObject.bottom + window.scrollY,
     left: rectObject.left + window.scrollX,
   };
+  this.center = {
+    x: (this.offset.left + this.offset.right) / 2,
+    y: (this.offset.top + this.offset.bottom) / 2,
+  }
 }
 
 Rect.prototype = {
@@ -67,6 +71,8 @@ function onScroll(event) {
 }
 
 function updateCanvas() {
+  rulerList.splice(0, rulerList.length);
+  guideList.splice(0, guideList.length);
   // 接触判定
   if (isCollided()) {
     const outer = base.calcArea() >= target.calcArea() ? base : target;
@@ -75,7 +81,10 @@ function updateCanvas() {
     if (isContained(outer, inner)) {
       console.log("contained.");
       // 内包される側を基点として線を描画する
-      updateRuler(inner, outer);
+      pushLine(rulerList, inner.center.x, inner.offset.top, inner.center.x, outer.offset.top);
+      pushLine(rulerList, inner.center.x, inner.offset.bottom, inner.center.x, outer.offset.bottom);
+      pushLine(rulerList, inner.offset.left, inner.center.y, outer.offset.left, inner.center.y);
+      pushLine(rulerList, inner.offset.right, inner.center.y, outer.offset.right, inner.center.y);
     } else {
       // 接触している
       console.log("collided.");
@@ -84,13 +93,72 @@ function updateCanvas() {
     // 接触していない
     console.log("not collided.");
 
-    // 基点の軸が当たる
-    // 基点の軸が当たらず、他方の軸が当たる
-    // 基点、他方の軸が当たらず、基点の辺から伸ばす
-    // 基点の辺から伸ばす線が当たらない
+    // 位置関係を判定する
+    const isTopPosition = base.offset.top >= target.offset.bottom;
+    const isRightPosition = base.offset.right <= target.offset.left;
+    const isBottomPosition = base.offset.bottom <= target.offset.top;
+    const isLeftPosition = base.offset.left >= target.offset.right;
+    /*
+    let pos = isTopPosition ? "Top" : "";
+    pos += isRightPosition ? "Right" : ""
+    pos += isBottomPosition ? "Bottom" : "";
+    pos += isLeftPosition ? "Left" : "";
+    console.log(pos);
+    */
+
+    if (isLeftPosition || isRightPosition) {
+      let y;
+      if (target.offset.top <= base.center.y && base.center.y <= target.offset.bottom) {
+        // 基点の軸が当たる
+        y = base.center.y;
+      } else if (base.offset.top <= target.center.y && target.center.y <= base.offset.bottom) {
+        //基点の軸が当たらず、他方の軸が当たる
+        y = target.center.y;
+      } else if ((target.offset.top <= base.offset.top && base.offset.top <= target.offset.bottom) || (target.offset.top <= base.offset.bottom && base.offset.bottom <= target.offset.bottom)) {
+        // 基点、他方の軸が当たらず、基点の辺の延長線が当たる
+        y = Math.abs(base.offset.top - target.center.y) < Math.abs(base.offset.bottom - target.center.y) ? base.offset.top : base.offset.bottom;
+      } else {
+        // 基点の辺の延長線が当たらない
+        // 線自体は基点の軸が当たるケースと同じ
+        y = base.center.y;
+        // ガイド線が必要
+        const guideEndY = Math.abs(base.center.y - target.offset.top) < Math.abs(base.center.y - target.offset.bottom) ? target.offset.top : target.offset.bottom;
+        const guideX = isLeftPosition ? target.offset.right - 1 : target.offset.left;
+        pushLine(guideList, guideX, base.center.y, guideX, guideEndY);
+      }
+      const start = isLeftPosition ? "left" : "right";
+      const end = isLeftPosition ? "right" : "left";
+      pushLine(rulerList, base.offset[start], y, target.offset[end], y);
+    }
+    if (isTopPosition || isBottomPosition) {
+      let x;
+      if (target.offset.left <= base.center.x && base.center.x <= target.offset.right) {
+        // 基点の軸が当たる
+        x = base.center.x;
+      } else if (base.offset.left <= target.center.x && target.center.x <= base.offset.right) {
+        //基点の軸が当たらず、他方の軸が当たる
+        x = target.center.x;
+      } else if ((target.offset.left <= base.offset.left && base.offset.left <= target.offset.right) || (target.offset.left <= base.offset.right && base.offset.right <= target.offset.right)) {
+        // 基点、他方の軸が当たらず、基点の辺の延長線が当たる
+        x = Math.abs(base.offset.left - target.center.x) < Math.abs(base.offset.right - target.center.x) ? base.offset.left : base.offset.right;
+      } else {
+        // 基点の辺の延長線が当たらない
+        // 線自体は基点の軸が当たるケースと同じ
+        x = base.center.x;
+        // ガイド線が必要
+        const guideEndX = Math.abs(base.center.x - target.offset.left) < Math.abs(base.center.x - target.offset.right) ? target.offset.left : target.offset.right;
+        const guideY = isTopPosition ? target.offset.bottom - 1 : target.offset.top;
+        pushLine(guideList, base.center.x, guideY, guideEndX, guideY);
+      }
+      const start = isTopPosition ? "top" : "bottom";
+      const end = isTopPosition ? "bottom" : "top";
+      pushLine(rulerList, x, base.offset[start], x, target.offset[end]);
+    }
   }
   // 再描画
+  context.clearRect(0, 0, canvasWrapper.offsetWidth, canvasWrapper.offsetHeight);
   drawRuler();
+  drawGuide();
 }
 
 function isCollided() {
@@ -104,14 +172,6 @@ function isContained(outer, inner) {
     (outer.offset.left <= inner.offset.left && inner.offset.left <= outer.offset.right) &&
     (outer.offset.left <= inner.offset.right && inner.offset.right <= outer.offset.right)
   );
-}
-
-function updateRuler(a, b) {
-  rulerList.splice(0, rulerList.length);
-  pushLine(rulerList, (a.offset.left + a.offset.right) / 2, a.offset.top, (a.offset.left + a.offset.right) / 2, b.offset.top);
-  pushLine(rulerList, (a.offset.left + a.offset.right) / 2, a.offset.bottom, (a.offset.left + a.offset.right) / 2, b.offset.bottom);
-  pushLine(rulerList, a.offset.left, (a.offset.top + a.offset.bottom) / 2, b.offset.left, (a.offset.top + a.offset.bottom) / 2);
-  pushLine(rulerList, a.offset.right, (a.offset.top + a.offset.bottom) / 2, b.offset.right, (a.offset.top + a.offset.bottom) / 2);
 }
 
 function pushLine(dest, startX, startY, endX, endY) {
@@ -132,9 +192,16 @@ function pushLine(dest, startX, startY, endX, endY) {
 }
 
 function drawRuler() {
-  context.clearRect(0, 0, canvasWrapper.offsetWidth, canvasWrapper.offsetHeight);
-  rulerList.forEach(function(ruler) {
+  context.setLineDash([]);
+  rulerList.forEach(function (ruler) {
     drawLine(ruler.start.x, ruler.start.y, ruler.end.x, ruler.end.y);
+  });
+}
+
+function drawGuide() {
+  context.setLineDash([3, 3]);
+  guideList.forEach(function(guide) {
+    drawLine(guide.start.x, guide.start.y, guide.end.x, guide.end.y);
   });
 }
 
@@ -147,7 +214,5 @@ function drawLine(startX, startY, endX, endY) {
   context.beginPath();
   context.moveTo(startX + window.scrollX + extraX, startY + window.scrollY + extraY);
   context.lineTo(endX + window.scrollX + extraX, endY + window.scrollY + extraY);
-  context.closePath();
   context.stroke();
-  console.count("draw");
 }
